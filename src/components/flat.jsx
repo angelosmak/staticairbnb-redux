9d9ef9a06bbce46cc7e8c2fc86c2fcaff3dc4f42
Angelos Makris <sade-1995@hotmail.com>
2023-10-04 21:12:52 +0300

    added containers components and actions

---
+++ src/components/flat.jsx
@@ -0,0 +1,19 @@
+import React , { Component } from "react";
+
+class Flat extends Component {
+  render () {
+    return (
+      <div className="card-trip">
+        <img src={this.props.flat.imageUrl} />
+        <div className="card-trip-infos">
+          <div>
+            <h2>{this.props.flat.name}</h2>
+            <p>{this.props.flat.price} {this.props.flat.priceCurrency}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Flat;
